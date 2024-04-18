70832f07a3db73e14f84467e4f31bf0f5276e153
UPZ <smilish67@naver.com>
2024-04-18 17:51:33 +0900

    solve later

---
+++ InterviewPrep150/Array/merge_sorted_array.py
@@ -0,0 +1,9 @@
+# my fisrt attempt was filing nums1's empty areas with nums2. and sorting using selection sort
+# time-complexity : O(n^2)
+# Another method could be different sorting algorithms like merge. (O(n*logN))
+
+# This problem asks conquer process since it gives already sorted two divided arrays.
+
+# So my second attempt was two pointer solving conquer process sequentially.
+# But sequential two pointer was hard, because it seemed constrained for using other array.
+# pushing is needed
